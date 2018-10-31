959f6c176b42da78d63719c84f5d0f6d226f6c4c
Killfill0o <phil3229@stud.kea.dk>
2018-10-31 16:31:27 +0100

    Heartbeat implemented.
    Missing:
    Multiple recievers of server message.

--- src/MultiEchoClient.java
+++ src/MultiEchoClient.java
@@ -1,78 +1,58 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.regex.Matcher;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class MultiEchoClient {
-    private static InetAddress host;
     private static final int PORT = 1235;
 
     public static void main(String[] args) {
-        try {
-            host = InetAddress.getLocalHost();
-        } catch (UnknownHostException uhEx) {
-            System.out.println("\nHost ID not found!\n");
-            System.exit(1);
-        }
-        sendMessages();
-
-        /*
-
-        startHeartBeat();
-
-        */
-    }
-
-/*
-    private static synchronized void startHeartBeat(){
-        try{
-            System.out.println("\ntest");
-            Socket socket = new Socket(InetAddress.getByName("192.168.0.14"), 1238);
-            System.out.println(socket.getInetAddress().getHostAddress()+socket.getPort());
-            Timer t = new Timer();
-
-            t.scheduleAtFixedRate(new TimerTask() {
-                public void run() {
-                    PrintWriter heartbeat = null;
-
-                    try {
-                        heartbeat = new PrintWriter(socket.getOutputStream(), true);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    assert heartbeat != null;
-
-                    heartbeat.println("IMAV");
-                }
-            }, 0, 60000);
-        }catch (Exception e){
-            System.out.println(""+e);
-        }
-    }
-*/
-int i = 0;
-
-    private static void sendMessages() {
         Socket socket = null;
-        Boolean accepted = false;
+        try {
+            socket = new Socket(InetAddress.getByName("192.168.0.14"), PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Heartbeat t1 = new Heartbeat("T1",socket);
+        t1.start();
+
+        /*RecievedMessage R1 = new RecievedMessage( "Thread-1");*/
+        SendMessage S1 = new SendMessage("Thread-2",socket);
+        /*R1.start();*/
+        S1.run();
+    }
+}
+
+class SendMessage implements Runnable {
+        private Thread t;
+        private String threadName;
+        private Socket socket;
+        private static final int PORT = 1235;
+
+        SendMessage(String name, Socket s) {
+            threadName = name;
+            System.out.println("Creating " +  threadName );
+            socket = s;
+        }
+
+        public void run() {
+        boolean accepted = false;
 
         try {
             String message = "", respons;
             String username = "";
+
             do {
                 if (username.equals("")) {
                     System.out.println("Please enter username (Username is max 12 chars long, only " + "letters, digits, ‘-‘ and ‘_’ allowed.\n");
                     Scanner username_input = new Scanner(System.in);
                     String username_string = username_input.next();
 
-                    socket = new Socket(InetAddress.getByName("192.168.0.14"), PORT);
                     if (username_string.length() <= 12) {
                         System.out.println(username_string + " is bellow 12 characters");
                         if (Pattern.matches("[a-zA-Z0-9]+", username_string)) {
@@ -82,62 +62,56 @@ int i = 0;
                             String ip = socket.getLocalAddress().toString();
                             String port = Integer.toString(socket.getLocalPort());
                             PrintWriter usernameOutput = new PrintWriter(socket.getOutputStream(), true);
-                            String joinOutput = "JOIN "+username+", "+ip+":"+port;
+                            String joinOutput = "JOIN " + username + ", " + ip + ":" + port;
                             usernameOutput.println(joinOutput);
 
                             Scanner j_OK = new Scanner(socket.getInputStream());
                             String joinInput = j_OK.next();
-                            if(joinInput.equals("J_OK")){
-                               accepted = true;
+                            if (joinInput.equals("J_OK")) {
+                                accepted = true;
                             }
-                            if(joinInput.matches("J_ER (.*):(.*)")){
+                            if (joinInput.matches("J_ER (.*):(.*)")) {
                                 accepted = false;
                             }
-                        }
-                        else{
+                        } else {
                             System.out.println(username_string + " is not allowed!");
                         }
                     }
                 } else {
-                    if(accepted){
-
+                    if (accepted) {
                         System.out.println("Type message and press enter to send (Max 255 characters)");
 
-                    Scanner userEntry = new Scanner(System.in);
+                        Scanner userEntry = new Scanner(System.in);
 
-                    Scanner messageInput = new Scanner(socket.getInputStream());
-                    PrintWriter messageOutput = new PrintWriter(socket.getOutputStream(), true);
+                        Scanner messageInput = new Scanner(socket.getInputStream());
+                        PrintWriter messageOutput = new PrintWriter(socket.getOutputStream(), true);
 
-                    String userInput = userEntry.nextLine();
-                    if(userInput.length()<=255) {
-                        message = "DATA " + username + ": " + userInput;
-                        System.out.println(message);
+                        String userInput = userEntry.nextLine();
+                        if (userInput.length() <= 255) {
+                            message = "DATA " + username + ": " + userInput;
+                            System.out.println(message);
 
-                        if (message.matches("DATA (.*): (.*)")) {
-                            messageOutput.println(message);
+                            if (message.matches("DATA (.*): (.*)")) {
 
-                            respons = messageInput.nextLine();
-                            if (respons.matches("DATA (.*): (.*)")) {
-                                System.out.println("\nSERVER> " + respons);
+                                messageOutput.println(message);
+
+                                respons = messageInput.nextLine();
+                                if (respons.matches("DATA (.*): (.*)")) {
+                                    System.out.println("\nSERVER> " + respons);
+                                }
                             }
+                        } else {
+                            System.out.println("Message To Long");
                         }
-                    }
-                    else{
-                        System.out.println("Message To Long");
-                    }
-                    }
-                    else {
+                    } else {
                         System.out.println("Client Not Accepted To Server");
                         message = "QUIT";
-                        sendMessages();
                     }
                 }
 
             }
-            while (!message.equals("QUIT")) ;
-        }
-        catch(IOException ioEx)
-        {
+            while (!message.equals("QUIT"));
+        } catch (IOException ioEx) {
             ioEx.printStackTrace();
         } finally {
             try {
@@ -150,5 +124,77 @@ int i = 0;
             }
         }
     }
+}
+/*
 
+class RecievedMessage implements Runnable {
+    private Thread t;
+    private String threadName;
+    private static final int PORT = 1235;
+
+    RecievedMessage(String name) {
+        threadName = name;
+        System.out.println("Creating " +  threadName );
+    }
+
+    public void run() {
+        System.out.println("Running " +  threadName );
+        String message = "", respons;
+        try {
+            Socket socket = new Socket(InetAddress.getByName("192.168.0.14"), PORT);
+            do {
+                    Scanner messageInput = new Scanner(socket.getInputStream());
+                    respons = messageInput.nextLine();
+                    if (respons.matches("DATA (.*): (.*)")) {
+                        System.out.println("\nSERVER> " + respons);
+                    }
+            }
+            while (!message.equals("QUIT"));
+                System.out.println("Thread: " + threadName + ", ");
+                // Let the thread sleep for a while.
+                Thread.sleep(50);
+            } catch (InterruptedException | IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    public void start () {
+        System.out.println("Starting " +  threadName );
+        if (t == null) {
+            t = new Thread (this, threadName);
+            t.start ();
+        }
+    }
+}
+
+*/
+
+
+class Heartbeat extends Thread{
+    String tname;
+    Socket socket;
+    String message;
+
+    Heartbeat(String string, Socket s){
+        tname = string;
+        socket  = s;
+    }
+
+    public void run() {
+        try {
+            System.out.println(tname+" is running!");
+            for(int i = 0; i < 31;){
+                Thread.sleep(1000);
+                if(i == 30){
+                    /*System.out.println(tname+" is alive!");*/
+                    PrintWriter imavOutput = new PrintWriter(socket.getOutputStream(), true);
+                    message = "IMAV";
+                    imavOutput.println(message);
+                    i = 0;
+                }
+                i++;
+            }
+        }catch (Exception e){
+        }
+    }
 }
--- src/MultiEchoServer.java
+++ src/MultiEchoServer.java
@@ -2,11 +2,10 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.*;
-import java.sql.DriverManager;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class MultiEchoServer {
     private static ServerSocket serverSocket;
@@ -24,62 +23,57 @@ public class MultiEchoServer {
             System.out.println("SERVER || main");
             Socket client = serverSocket.accept();
             String joinInput = new Scanner(client.getInputStream()).nextLine();
-            System.out.println("\n Protocol check @ JOIN <USERNAME>, <IP>:<PORT> | "+joinInput.matches("JOIN(.*),(.*):(.*)")+"\n");
 
             if(joinInput.matches("JOIN(.*),(.*):(.*)")) {
 
-                String[] parts1 = joinInput.split("(?=,)", 2);
-                String part1 = parts1[0].substring(5);
-                String part2 = parts1[1].substring(2);
-                System.out.println(" Username: "+part1);
-                String[] parts2 = part2.split("(?=:)", 5);
-                String part3 = parts2[0].substring(1);
-                System.out.println(" IP: "+part3);
-                String part4 = parts2[1].substring(1);
-                System.out.println(" PORT: "+part4);
+                    System.out.println("\n Protocol check @ JOIN <USERNAME>, <IP>:<PORT> | "+joinInput.matches("JOIN(.*),(.*):(.*)")+"\n");
+                    String[] parts1 = joinInput.split("(?=,)", 2);
+                    String part1 = parts1[0].substring(5);
+                    String part2 = parts1[1].substring(2);
+                    System.out.println("\n Username: " + part1);
+                    String[] parts2 = part2.split("(?=:)", 5);
+                    String part3 = parts2[0].substring(1);
+                    System.out.println(" IP: " + part3);
+                    String part4 = parts2[1].substring(1);
+                    System.out.println(" PORT: " + part4 + "\n");
 
-                System.out.println("\n Client : " + part3);
-                System.out.println(" Username : " + part1+"\n");
-                ClientObject clientObject = new ClientObject();
-                clientObject.setUsername(part1);
-                clientObject.setIp(part3);
-                clientObject.setPort(part4);
-                clientObject.setI(0);
-                clientList.add(clientObject);
+                    ClientObject clientObject = new ClientObject();
+                    clientObject.setUsername(part1);
+                    clientObject.setIp(part3);
+                    clientObject.setPort(part4);
+                    clientObject.setI(0);
+                    clientList.add(clientObject);
 
-                System.out.println(clientList.size());
-                if(clientList.size() == 1) {
-                    PrintWriter j_OK = new PrintWriter(client.getOutputStream(), true);
-                    j_OK.println("J_OK");
-                    System.out.println("FIRST!");
-                }
-                for(int i = 0; i < clientList.size()-1;){
+                    if (clientList.size() == 1) {
+                        PrintWriter j_OK = new PrintWriter(client.getOutputStream(), true);
+                        j_OK.println("J_OK");
+                    }
+                    for (int i = 0; i < clientList.size() - 1; ) {
                         if (part1.equals(clientList.get(i).getUsername())) {
                             PrintWriter j_ER = new PrintWriter(client.getOutputStream(), true);
                             j_ER.println("J_ER DUN: Duplicate Username");
-                            System.out.println("WOOPS!");
-                            clientList.remove(clientList.size()-1);
+                            clientList.remove(clientList.size() - 1);
                             break;
+                        } else {
+                            i++;
                         }
-                        else{i++;}
-                            if(i == clientList.size()-1){
+                        if (i == clientList.size() - 1) {
                             PrintWriter j_OK = new PrintWriter(client.getOutputStream(), true);
                             j_OK.println("J_OK");
-                            System.out.println("YAY!");
                         }
-                }
+                    }
 
 
-                System.out.println("\n New Client Accepted. \n");
-                System.out.println("▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                    System.out.println("\n New Client Accepted. \n");
+                    System.out.println("▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬");
 
-                for (int i = 0; i < clientList.size(); ) {
-                    System.out.println(i + " " + clientList.get(i).getUsername() + " " + clientList.get(i).getIp() + ":" + clientList.get(i).getPort());
-                    i++;
-                }
-                System.out.println("▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬");
-                ClientHandler handler = new ClientHandler(client);
-                handler.start();
+                    for (int i = 0; i < clientList.size(); ) {
+                        System.out.println(i + " " + clientList.get(i).getUsername() + " " + clientList.get(i).getIp() + ":" + clientList.get(i).getPort());
+                        i++;
+                    }
+                    System.out.println("▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                    ClientHandler handler = new ClientHandler(client);
+                    handler.start();
             } else {
                 PrintWriter j_ER = new PrintWriter(client.getOutputStream(), true);
                 j_ER.println("J_ER CNA: Client Not Accepted");
@@ -89,44 +83,43 @@ public class MultiEchoServer {
     }
 }
 
-class ChatServerThread extends Thread {
-    private Socket socket = null;
-    private int ID = -1;
-    private DataInputStream streamIn = null;
-
-    public ChatServerThread(Socket _socket) {
-        socket = _socket;
-        ID = socket.getPort();
-    }
-}
-
 class ClientHandler extends Thread {
     private Socket client;
     private Scanner input;
     private PrintWriter output;
 
-    public ClientHandler(Socket socket) {
+    public ClientHandler(Socket socket) throws IOException {
         client = socket;
         System.out.println("SERVER || ClientHandler");
         try {
             input = new Scanner(client.getInputStream());
             output = new PrintWriter(socket.getOutputStream(), true);
-        } catch (IOException ioEx) {
+        }
+        catch (IOException ioEx) {
             ioEx.printStackTrace();
         }
+
     }
 
     public void run() {
         String recieved;
 
         System.out.println("SERVER || run");
+
         do {
             recieved = input.nextLine();
-            if (recieved.matches("DATA (.*): (.*)")) {
+
+            if (recieved.matches("DATA (.*): (.*)")||recieved.equals("IMAV")) {
+                if(recieved.equals("IMAV")){
+                    System.out.println(client.getInetAddress().toString()+":"+client.getPort()+" is alive!");
+                    System.out.println("\n Protocol check @ IMAV | "+recieved.matches("IMAV")+"\n");
+                }else{
                 String[] parts1 = recieved.split("(?=:)", 2);
                 String part1 = parts1[0].substring(5);
                 String part2 = parts1[1].substring(2);
                 output.println("DATA "+part1+": "+part2);
+                System.out.println("SERVER > "+part1+": "+part2);
+                }
             }
         }
         while (!recieved.equals("QUIT"));
@@ -142,45 +135,3 @@ class ClientHandler extends Thread {
     }
 
 }
-
-/*
-
-class IMAV extends Thread{
-    private Socket client;
-    private Scanner input;
-    private PrintWriter output;
-    private int seconds = 60;
-
-   public IMAV(ServerSocket serverSocket, Socket socket) {
-        System.out.println("1");
-        do {
-
-            System.out.println("2");
-            try {
-                client = serverSocket.accept();
-                input = new Scanner(client.getInputStream());
-
-                Timer timer = new Timer();
-                timer.scheduleAtFixedRate(new TimerTask() {
-                    @Override
-                    public void run() {
-                        System.out.println("Are you alive?");
-                        if(input.next().equals("IMAV")){
-                            System.out.println("yes");
-                            System.out.println("test"+input.next());
-                        }
-                    }
-                }, 0, 1000);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            System.out.println("3");
-
-        } while (!input.next().equals("QUIT"));
-
-
-    }
-}
-
-*/
